797956426d192eff7396bb075392fe21ba00336d
yanica svensson <sweets86_@hotmail.com>
2020-05-20 00:48:21 +0200

    nodemon

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
-
 const app = express()
-const port = 4000
+const expressLayouts = require('express-ejs-layouts')
 
-app.get('/', (req, res, next) => {
-    res.send('Hi')
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
 })
 
-app.listen(port, () => {
-    console.log('The app is running on ' + port)
-})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 4000)
